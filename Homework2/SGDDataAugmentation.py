bc08a86a2344192e7ac90c2133e004ebd0b7803e
DragosBadarau <74381554+DragosBadarau@users.noreply.github.com>
2024-10-21 19:39:58 +0300

    homework 2

---
+++ Homework2/SGDDataAugmentation.py
@@ -0,0 +1,208 @@
+import torch
+from torch import nn, Tensor
+from torch.utils.data import Dataset, DataLoader
+from torchvision.datasets import CIFAR100
+import pandas as pd
+from torchvision.transforms import v2
+from torch.backends import cudnn
+from torch import GradScaler
+from torch import optim
+from tqdm import tqdm
+
+device = torch.device('cuda')
+cudnn.benchmark = True
+pin_memory = True
+enable_half = True  # Disable for CPU, it is slower!
+scaler = GradScaler(device, enabled=enable_half)
+
+
+class SimpleCachedDataset(Dataset):
+    def __init__(self, dataset):
+        # Runtime transforms are not implemented in this simple cached dataset.
+        self.data = tuple([x for x in dataset])
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, i):
+        return self.data[i]
+
+
+basic_transforms = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.Normalize((0.5, 0.5, 0.5), (0.25, 0.25, 0.25), inplace=True)
+])
+train_transforms = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.RandomRotation(degrees=10),  # Mild rotation to add more variety
+    v2.RandomHorizontalFlip(p=0.25),
+    v2.Normalize((0.5, 0.5, 0.5), (0.25, 0.25, 0.25), inplace=True)
+])
+
+train_set = CIFAR100('/kaggle/input/fii-atnn-2024-assignment-2', download=True, train=True, transform=train_transforms)
+test_set = CIFAR100('/kaggle/input/fii-atnn-2024-assignment-2', download=True, train=False, transform=basic_transforms)
+train_set = SimpleCachedDataset(train_set)
+test_set = SimpleCachedDataset(test_set)
+
+train_loader = DataLoader(train_set, batch_size=64, shuffle=True, pin_memory=pin_memory)
+test_loader = DataLoader(test_set, batch_size=500, pin_memory=pin_memory)
+
+
+class VGG16(nn.Module):
+    def __init__(self):
+        super(VGG16, self).__init__()
+
+        self.layers = nn.Sequential(
+            # Block 1
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # Block 2
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # Block 3
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # Block 4
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # Block 5
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # Classifier
+            nn.Flatten(),
+            nn.Linear(512, 100)
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.layers(x)
+
+
+model = VGG16().to(device)
+model = torch.jit.script(model)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=0.01)
+def train():
+    model.train()
+    correct = 0
+    total = 0
+
+    for inputs, targets in train_loader:
+        inputs, targets = inputs.to(device, non_blocking=True), targets.to(device, non_blocking=True)
+        with torch.autocast(device.type, enabled=enable_half):
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+        optimizer.zero_grad()
+
+        predicted = outputs.argmax(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+    return 100.0 * correct / total
+
+
+@torch.inference_mode()
+def val():
+    model.eval()
+    correct = 0
+    total = 0
+
+    for inputs, targets in test_loader:
+        inputs, targets = inputs.to(device, non_blocking=True), targets.to(device, non_blocking=True)
+        with torch.autocast(device.type, enabled=enable_half):
+            outputs = model(inputs)
+
+        predicted = outputs.argmax(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+    return 100.0 * correct / total
+
+
+@torch.inference_mode()
+def inference():
+    model.eval()
+
+    labels = []
+
+    for inputs, _ in test_loader:
+        inputs = inputs.to(device, non_blocking=True)
+        with torch.autocast(device.type, enabled=enable_half):
+            outputs = model(inputs)
+
+        predicted = outputs.argmax(1).tolist()
+        labels.extend(predicted)
+
+    return labels
+
+
+best = 0.0
+patience = 11  # Number of epochs to wait for improvement
+epochs_no_improve = 0
+epochs = list(range(50))
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.25, patience=3)
+
+with tqdm(epochs) as tbar:
+    for epoch in tbar:
+        train_acc = train()
+        val_acc = val()
+        scheduler.step(val_acc)
+        if val_acc > best:
+            best = val_acc
+            epochs_no_improve = 0
+        else:
+            epochs_no_improve += 1
+
+        if epochs_no_improve == patience:
+            print("Early stopping due to no improvement in validation accuracy.")
+            break
+        tbar.set_description(f"Train: {train_acc:.2f}, Val: {val_acc:.2f}, Best: {best:.2f}")
+data = {
+    "ID": [],
+    "target": []
+}
+
+for i, label in enumerate(inference()):
+    data["ID"].append(i)
+    data["target"].append(label)
+
+df = pd.DataFrame(data)
+df.to_csv("/kaggle/working/submission.csv", index=False)
